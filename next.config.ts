fe298204637772aad629ecf0ff0a796ce8e04598
wrdsnf <wardafawahana@gmail.com>
2026-02-02 11:40:01 +0700

    prepare netlify deploy

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
